b5d4ed431edc187ac32cb7febe5375915625356f
eduardoliveirap <117646670+eduardoliveirap@users.noreply.github.com>
2023-03-28 14:06:30 -0300

    done project

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
-// let listaDeCompras = ["ovos", "leite", "manteiga"];
+let nRandom = Math.floor(Math.random() * 100) + 1
+let palp = document.getElementById('txtpalp')
+let res = document.getElementById('lista-palp')
+let input = document.getElementById('input')
+let imgBia = document.getElementById('bia')
+let butn = document.createElement('BUTTON')
+let listPalp = []
 
-// let listaDeCompras = [] ;
-// listaDeCompras[0] = "ovos";
-// listaDeCompras[1] = "leite";
-// listaDeCompras[200] = "farinha"; 
+//Adiciona uma function ao botão criado no JS
+butn.addEventListener('click', function(){document.location.reload()})
 
-// let listaDeCompras = [];
-
-// listaDeCompras.push("açucar");
-// listaDeCompras.push("feijao");
-// listaDeCompras.push("arroz");
-
-let tentativas = 0;
-let numeroRandomico = Math.floor(Math.random() * 100) + 1;
-
-function checarTentativa(){
-    tentativas++;
-
-    if(tentativas == 5){
-        alert("Game over");
-        perdeu();
-        return;
+//Verifica se é um numero
+function palpites(n){
+    if(Number(n)>=1 && Number(n) <=100){
+        return true
+    }else{
+        return false
     }
-    
-    alert(tentativas);
 }
 
-function perdeu(){
-    window.location.reload;
+//Verifica se ta na lista
+function inList(n,l){
+    if(l.indexOf(Number(n)) == -1){
+        return true
+    }else{        
+        alert('Ja fez esse palpite')
+        return false
+    }
 }
 
+function ganhar(){
+    if(Number(palp.value)==nRandom){ 
+        butn.innerHTML = 'Jogar novamente'
+        imgBia.src = '/images/biaGanha.png'
+        input.innerHTML = `<h3>Parabéns! Você acertou, o número é: <span>${nRandom}</span></h3>`               
+        input.appendChild(butn)
+    }
+    else{
+        return false
+    }
+}
+function perder(){
+    if(ganhar()==false && listPalp.length == 10){    
+        butn.innerHTML = 'Tente novamente'
+        imgBia.src = '/images/biaPerde.png'
+        input.innerHTML = `<h3>Você não acertou, o número era: <span>${nRandom}</span></h3>`
+        input.appendChild(butn)      
+    }else{                
+        return false
+    }
+}
+
+//Onde o jogo acontece
+function jogo(){
+    document.getElementById('enter').innerHTML = ''
+    if(inList(palp.value, listPalp) && palpites(palp.value)){ 
+        ganhar()           
+        res.innerHTML += `${palp.value}` 
+        listPalp.push(Number(palp.value))
+        if(Number(palp.value) > nRandom){
+            alert('Maior do que pensei')
+        }
+        else if(Number(palp.value) < nRandom){
+            alert('Menor do que pensei')
+        }         
+        perder()  
+        if(perder() == false && ganhar() == false){
+            res.innerHTML += ' - '
+        }
+    }
+}
+
+//Apertar Enter e apagar o input
+function enviar(){
+    if(event.keyCode == 13){
+        jogo()   
+    }else{
+        return false
+    }      
+    palp.value = "" 
+}
